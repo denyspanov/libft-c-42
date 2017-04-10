4d48788303d3f355f9de884eefc4c1468c3da7cb
Denys <denys.panov@gmail.com>
2017-04-10 12:39:44 +0300

    updated

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -12,17 +12,17 @@
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void *ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t i;
+	size_t bytes;
 
-	i = 0;
-	while (i < n)
+	bytes = 0;
+	while (bytes < n)
 	{
-		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-		if (((unsigned char *)src)[i] == (unsigned char)c)
-			return (&dst[i + 1]);
-		i++;
+		*((unsigned char *)dst + bytes) = *((unsigned char *)src + bytes);
+		if (*((unsigned char *)src + bytes) == (unsigned char)c)
+			return ((unsigned char *)dst + bytes + 1);
+		bytes++;
 	}
 	return (NULL);
 }
--- ft_memchr.c
+++ ft_memchr.c
@@ -12,18 +12,19 @@
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t n)
+void				*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
+	unsigned char	*cpy;
 
 	i = 0;
-	while (i < n)
-	{
-		if (((unsigned char *)s)[i] == (unsigned char)c)
+	cpy = (unsigned char *)s;
+	if (s && n > 0)
+		while (i < n)
 		{
-			return ((void *)&s[i]);
+			if (cpy[i] == (unsigned char)c)
+				return ((void*)&cpy[i]);
+			i++;
 		}
-		i++;
-	}
 	return (NULL);
 }
